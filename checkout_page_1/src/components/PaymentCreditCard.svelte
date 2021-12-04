fc80804128e77276faff62be2273ba6de9342fb4
Muhamad Hilmy Haidar <hilmy_21@apps.ipb.ac.id>
2021-12-04 12:17:35 +0700

    wip

--- checkout_page_1/src/components/PaymentCreditCard.svelte
+++ checkout_page_1/src/components/PaymentCreditCard.svelte
@@ -2,7 +2,7 @@
   import Input from "./Input.svelte"
 </script>
 
-<form autocomplete="off" class="credit-card">
+<form autocomplete="off" class="credit-card" role="presentation">
   <Input type="text" name="credit-card-name" label="Cardholder Name" />
   <Input type="text" name="credit-card-number" label="Card Number" />
   <Input type="text" name="credit-card-exp-date" label="Expire Date" />
